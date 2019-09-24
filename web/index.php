1ee126eb1a47903758af2da627c22518086be287
Shane <shanedavenport15@gmail.com>
2019-09-24 15:52:55 -0600

    Hello

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
-    $colors = array(0 => "red", 1 => "blue");
+    $colors = array("red", "blue", "green");
     for ($x = 1; $x <= 10; $x++) {
-        echo "<div style='color:" . $colors[1] . ";'>Hello</div>";
+        echo "<div style='color:" . $colors[$x % 3] . ";'>Hello</div>";
     }
 ?>
