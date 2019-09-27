57a80386b71a73572d0f9b1bc73c810c32a373e8
Shane <shanedavenport15@gmail.com>
2019-09-26 22:11:42 -0600

    I did a lot of things.

--- web/index.php
+++ web/index.php
@@ -1,6 +1,3 @@
 <?php
-    $colors = array("red", "blue", "green");
-    for ($x = 1; $x <= 10; $x++) {
-        echo "<div style='color:" . $colors[$x % 3] . ";'>Hello</div>";
-    }
+    echo "Hello, World!";
 ?>
