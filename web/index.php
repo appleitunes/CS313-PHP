827ee66f6deb42a4a1f8f72ac9e55d28d10e3156
Shane <shanedavenport15@gmail.com>
2019-09-29 16:50:31 -0600

    sdf

--- web/index.php
+++ web/index.php
@@ -4,6 +4,16 @@
         $rawData = explode("?q=", $link)[1];
         $splitData = explode("+", $rawData);
         $data = implode(" ", $splitData);
+
+        for ($i = 0; $i < sizeof($data); $i++) {
+            if (rand(0,1) == 1) {
+                $data[$i] = strtoupper($data[$i]);
+            }
+            else {
+                $data[$i] = strtolower($data[$i]);
+            }
+        }
+
         echo $data;
     }
     catch (Exception $e) {
