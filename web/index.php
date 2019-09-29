12dfae0be8fac2c4ea51e263f2308d4474d3db7d
Shane <shanedavenport15@gmail.com>
2019-09-29 17:01:49 -0600

    sda

--- web/index.php
+++ web/index.php
@@ -5,16 +5,11 @@
         $splitData = explode("+", $rawData);
         $data = implode(" ", $splitData);
 
-        for ($i = 0; $i < sizeof($data); $i++) {
-            if (rand(0,1) == 1) {
-                $data[$i] = strtoupper($data[$i]);
-            }
-            else {
-                $data[$i] = strtolower($data[$i]);
-            }
+        $data = str_split(strtolower($data));
+        foreach ($data as &$char) {
+            if (rand(0, 1)) $char = strtoupper($char);
         }
-
-        echo $data;
+        echo implode("", $data);
     }
     catch (Exception $e) {
         echo $e->getMessage();
