e0e4de24e210794b108917d205cccbb66e2c973d
Shane <shanedavenport15@gmail.com>
2019-09-28 23:00:22 -0600

    Hello

--- web/index.php
+++ web/index.php
@@ -1,6 +1,7 @@
 <?php
     $link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-    $rawData = explode("%%", $link);
-    // $data = explode("&&", $rawData);
-    echo $rawData;
+    $rawData = explode("%%", $link)[1];
+    $splitData = explode("&&", $rawData);
+    $data = implode(" ", $splitData);
+    echo $data;
 ?>
