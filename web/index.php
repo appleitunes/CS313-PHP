dc5458602bef5e9a4fe30461bd95edb3fed1f2d7
Shane <shanedavenport15@gmail.com>
2019-09-28 22:49:47 -0600

    sdf

--- web/index.php
+++ web/index.php
@@ -1,7 +1,6 @@
 <?php
     $link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
     $rawData = explode("%%", $link)[1];
-    $data = explode("&&", $rawData);
-    $string = implode(" ", $data);
-    echo $string;
+    // $data = explode("&&", $rawData);
+    echo $rawData;
 ?>
