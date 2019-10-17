9510c5085c6207d86805b5ec94986a5058138d0b
Shane <shanedavenport15@gmail.com>
2019-10-17 14:52:10 -0600

    sdf

--- web/Typing/home.php
+++ web/Typing/home.php
@@ -1,12 +1,25 @@
+<?php
+    require "dbConnect.php";
+    $db = get_db();
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Scriptures</title>
+        <title>Test</title>
     </head>
     <body>
-        <h1>Scripture Resources</h1>
+        <?php
+            $statement = $db->prepare("SELECT book, chapter, verse, content FROM scripture");
+            $statement->execute();
+
+            while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+                $book = $row['book'];
+                $chapter = $row['chapter'];
+                $verse = $row['verse'];
+                $content = $row['content'];
+                echo "<p><strong>$book $chapter:$verse</strong> - \"$content\"<p>";
+            }
+        ?>
     </body>
 </html>
