1b0c3617c9c13db7f68f65ec0abb7179926a2067
Shane <shanedavenport15@gmail.com>
2019-10-17 15:37:20 -0600

    dsf

--- db/myDb.sql
+++ db/myDb.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS Stories;
 CREATE TABLE Stories (
     story_id        SERIAL              NOT NULL PRIMARY KEY
 ,   numRatings      INTEGER             NOT NULL
-,   totalRating     VARCHAR(255)        NOT NULL
+,   totalRating     INTEGER             NOT NULL
 ,   title           VARCHAR(255)        NOT NULL
 ,   story           VARCHAR(4096)       NOT NULL
 ,   author          VARCHAR(64)         NOT NULL
