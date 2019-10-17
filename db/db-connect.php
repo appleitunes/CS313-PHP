8b421af8df564ca2868e28e7dd9419a156f781c0
Shane <shanedavenport15@gmail.com>
2019-10-17 14:41:49 -0600

    test

--- db/db-connect.php
+++ db/db-connect.php
@@ -1,25 +1,17 @@
 <?php
     try {
         $dbUrl = getenv('DATABASE_URL');
-
         $dbOpts = parse_url($dbUrl);
-
         $dbHost = $dbOpts["host"];
         $dbPort = $dbOpts["port"];
         $dbUser = $dbOpts["user"];
         $dbPassword = $dbOpts["pass"];
-        $dbName = ltrim($dbOpts["path"],'/');
-
+        $dbName = ltrim($dbOpts["path"], '/');
         $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        echo "You're excused.";
-    }
-    catch (PDOException $ex) {
+        $_SESSION['db'] = $db;
+    } catch (PDOException $ex) {
         echo 'Error!: ' . $ex->getMessage();
         die();
-
-        echo "You done messed up Aaron!";
     }
 ?>
--- web/Typing/home.php
+++ web/Typing/home.php
@@ -1,22 +1,28 @@
-<?php require('../../db/dbConnect.php') ?>
+<?php 
+require 'db-connect.php';
+?>
 
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Test</title>
-    </head>
-    <body>
-        <?php
-            $statement = $db->prepare("SELECT book, chapter, verse, content FROM scripture");
-            $statement->execute();
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Scriptures</title>
+</head>
+<body>
 
-            while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-                $book = $row['book'];
-                $chapter = $row['chapter'];
-                $verse = $row['verse'];
-                $content = $row['content'];
-                echo "<p><strong>$book $chapter:$verse</strong> - \"$content\"<p>";
-            }
-        ?>
-    </body>
+<h1>Scripture Resources</h1>
+<?php
+foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures;') as $row) {
+    $book = $row['book'];
+    $chapter = $row['chapter'];
+    $verse = $row['verse'];
+    $content = $row['content'];
+    echo "<b>$book $chapter:$verse</b> - \"$content\"";
+    echo "<br/>";
+    echo "<br/>";
+}
+?>
+</body>
 </html>
