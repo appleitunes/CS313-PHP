45b39576e46264d914f7c2d75a3c38a4b120085f
Shane <shanedavenport15@gmail.com>
2019-10-18 21:03:11 -0600

    sdf

--- web/Typing/loadStory.php
+++ web/Typing/loadStory.php
@@ -1,14 +1,16 @@
 <?php
+    require "../../db/dbConnect.php";
+
     try {
         $link = "$_SERVER[REQUEST_URI]";
         $id = explode("?q=", $link)[1];
 
-        // foreach ($db->query("SELECT rating, comment FROM Reviews r WHERE r.story_id = '$id'") as $row) {
-        //     $rating = $row["rating"];
-        //     $comment = $row["comment"];
+        foreach ($db->query("SELECT rating, comment FROM Reviews r WHERE r.story_id = '$id'") as $row) {
+            $rating = $row["rating"];
+            $comment = $row["comment"];
 
-        //     echo "$comment<br>";
-        // }
+            echo "$comment<br>";
+        }
 
         echo $id;
     }
