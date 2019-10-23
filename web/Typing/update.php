dbe1960e19eebb946b2ec5457ea2acfcefcfbddd
Shane <shanedavenport15@gmail.com>
2019-10-23 17:31:37 -0600

    dsf

--- web/Typing/update.php
+++ web/Typing/update.php
@@ -5,15 +5,13 @@
             $content = $_POST["content_text"];
             $author = $_POST["author"];
 
-            echo "$title<br>$content<br>$author";
+            $sql = "INSERT INTO stories (title, story, author) VALUES ('$title', '$content', '$author');";
 
-            // $sql = "INSERT INTO stories (title, story, author) VALUES ('$title', '$content', '$author');";
-
-            // if ($db->query($sql) == TRUE) {
-            //     echo "New story uploaded successfully";
-            // } else {
-            //     echo "Error: Story not uploaded.";
-            // }
+            if ($db->query($sql) == TRUE) {
+                echo "New story uploaded successfully";
+            } else {
+                echo "Error: Story not uploaded.";
+            }
 
             // /* Redirect browser */
             // header("Location: main.php"); 
