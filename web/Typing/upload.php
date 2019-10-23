62cd092f1b6156041735bb0b34136350b49cded7
Shane <shanedavenport15@gmail.com>
2019-10-23 15:54:12 -0600

    df

--- web/Typing/upload.php
+++ web/Typing/upload.php
@@ -5,13 +5,13 @@
 
             echo $content;
 
-            // $sql = "INSERT INTO stories (title, story, author) VALUES ('test', '$content', 'test');";
+            $sql = "INSERT INTO stories (title, story, author) VALUES ('test', '$content', 'test');";
 
-            // if ($db->query($sql) == TRUE) {
-            //     echo "New story uploaded successfully";
-            // } else {
-            //     echo "Error: " . $sql . "<br>" . $db->error;
-            // }
+            if ($db->query($sql) == TRUE) {
+                echo "New story uploaded successfully";
+            } else {
+                echo "Error: " . $sql . "<br>" . $db->error;
+            }
         }
     }
     catch (Exception $e) {
