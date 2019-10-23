39ab7ac5967791292f3e6533a3fe42d7d0dac3a5
Shane <shanedavenport15@gmail.com>
2019-10-23 17:38:09 -0600

    df

--- web/Typing/submit.php
+++ web/Typing/submit.php
@@ -2,16 +2,20 @@
 <html>
     <head>
         <title>Submit Story</title>
+        <link rel="stylesheet" href="css/submit.css">
+        <link rel="stylesheet" href="css/main.css">
     </head>
     <body>
-        <form action="update.php" method="post">
-            <input name="author" type="text" placeholder="Author">
-            <br>
-            <input name="title" type="text" placeholder="Title">
-            <br>
-            <textarea name="content_text"><?php echo $_POST["content_text"]; ?></textarea>
-            <br>
-            <button type="submit">Submit</button>
-        </form>
+        <div class="container">
+            <form action="update.php" method="post">
+                <input name="author" type="text" placeholder="Author">
+                <br>
+                <input name="title" type="text" placeholder="Title">
+                <br>
+                <textarea name="content_text"><?php echo $_POST["content_text"]; ?></textarea>
+                <br>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
     </body>
 </html>
