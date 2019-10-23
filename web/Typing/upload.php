51e6fd3b275e02998dcbecd50f761c3fcd83254f
Shane <shanedavenport15@gmail.com>
2019-10-23 12:09:50 -0600

    sdf

--- web/Typing/upload.php
+++ web/Typing/upload.php
@@ -1,13 +1,13 @@
 <?php
+    echo "here i am";
+
     $content = $_post["text_area"];
 
-    if ($content) {
-        $sql = "INSERT INTO scriptures (title, story, author) VALUES ('test', '$content', 'test');";
+    $sql = "INSERT INTO scriptures (title, story, author) VALUES ('test', '$content', 'test');";
 
-        if ($db->query($sql) == TRUE) {
-            echo "New story uploaded successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . $db->error;
-        }
+    if ($db->query($sql) == TRUE) {
+        echo "New story uploaded successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $db->error;
     }
 ?>
