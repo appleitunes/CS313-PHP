2dd3e0a6c8f8ee074ac0df8f6f246634939e442c
Shane <shanedavenport15@gmail.com>
2019-10-23 18:43:08 -0600

    df

--- web/Typing/main.php
+++ web/Typing/main.php
@@ -9,7 +9,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <link rel="stylesheet" href="css/main.css">
-        <link rel="stylesheet" href="css/popup.css">
+        <link rel="stylesheet" href="css/story.css">
         <link rel="stylesheet" href="css/menu.css">
         <link rel="stylesheet" href="css/selection.css">
         <link rel="stylesheet" href="css/typing.css">
