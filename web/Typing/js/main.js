46e82cb8dd0d9a5370a4f01c1e1c9c03a2dd9831
Shane <shanedavenport15@gmail.com>
2019-10-26 19:25:56 -0600

    sdf

--- web/Typing/js/main.js
+++ web/Typing/js/main.js
@@ -1,5 +1,14 @@
 var i, start, countDown, interval, elements, finished, currID;
 
+function maxLength(el) {    
+    if (!('maxLength' in el)) {
+        var max = el.attributes.maxLength.value;
+        el.onkeypress = function () {
+            if (this.value.length >= max) return false;
+        };
+    }
+}
+
 window.onload = () => {
 
     document.getElementById("cancel").onclick = () => {
@@ -35,6 +44,8 @@ window.onload = () => {
             this.selectionEnd = s + 1; 
         }
     }
+
+    maxLength(elements["text"]);
 };
 
 /**
