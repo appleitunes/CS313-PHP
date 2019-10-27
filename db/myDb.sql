d1132ab6379e52d25a39abefd7503db4c096c60a
Shane <shanedavenport15@gmail.com>
2019-10-26 19:49:11 -0600

    dsf

--- db/myDb.sql
+++ db/myDb.sql
@@ -8,12 +8,10 @@ CREATE TABLE Stories (
 ,   title           VARCHAR(255)        NOT NULL
 ,   story           VARCHAR(4096)       NOT NULL
 ,   author          VARCHAR(64)         NOT NULL
-,   created_date    DATE                NOT NULL
 );
 
 CREATE TABLE Comment (
     comment_id      SERIAL              PRIMARY KEY
 ,   comment         VARCHAR(255)        NOT NULL
 ,   story_id        SERIAL              REFERENCES Stories (story_id)
-,   created_date    DATE                NOT NULL
 );
