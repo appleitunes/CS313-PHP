23697f764da015effbfb47c5881024f71e285b39
Shane <shanedavenport15@gmail.com>
2019-10-28 10:35:31 -0600

    dsf

--- web/Typing/comment.php
+++ web/Typing/comment.php
@@ -5,6 +5,10 @@
         $id = $_GET["id"];
         $comment = $_GET["comment"];
 
+        if (sizeof($comment) < 5) {
+            return;
+        }
+
         $sql = "INSERT INTO Comment (comment, story_id) VALUES ('$comment', $id);";
 
         if ($db->query($sql) == TRUE) {
