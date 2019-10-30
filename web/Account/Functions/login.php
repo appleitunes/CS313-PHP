9f5b1055c7c378d6ee424f6ef357389a8685d99a
Shane <shanedavenport15@gmail.com>
2019-10-30 00:03:15 -0600

    sdf

--- web/Account/Functions/login.php
+++ web/Account/Functions/login.php
@@ -6,7 +6,7 @@
 
     try {
         $username = $_GET["username"];
-        //$password = $_GET["password"];
+        // $password = $_GET["password"];
         $password = password_hash($_GET["password"], PASSWORD_DEFAULT);
 
         // Get the password and id of the item that matches this username
@@ -18,6 +18,11 @@
             if (password_verify($password, $row["pass"])) {
                 $_SESSION["id"] = $row["unique_id"];
                 echo "0";
+            }
+            else {
+                $id1 = $_SESSION["id"];
+                $id2 = $row["unique_id"];
+                echo "$id1 - $id2";
             }
         }
 
