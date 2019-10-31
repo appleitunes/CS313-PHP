a321636b8d17a93975d266bc8a94151ec747ef8a
Shane <shanedavenport15@gmail.com>
2019-10-30 22:02:51 -0600

    Changed Titles

--- web/Account/welcome.php
+++ web/Account/welcome.php
@@ -16,7 +16,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>Welcome</title>
 
         <script src="Javascript/account.js"></script>
 
